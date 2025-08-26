1a69fce07e47a7e6c0e71fc75a0177c2c88d9d90
Vincent Loechner <loechner@unistra.fr>
2025-08-26 15:42:15 +0200

    test makefiles clean

---
+++ Test/Tests.mk
@@ -0,0 +1,29 @@
+# Vincent Loechner, 2025
+
+tests:
+	@failedtest=0; \
+	failedtestnames=""; \
+	for x in $(TEST_FILES) ; do \
+		file=`basename $$x .in`; \
+		printf "Verify file $$file... " ; \
+		$(TEST_EXE)$(TEST_BITS) < $(srcdir)/$$x > xyz; \
+		diff -w xyz $(srcdir)/$$file.out ; \
+		result=$$?; \
+		if [ "$$result" -eq "0" ]; then \
+			printf "passed\n"; \
+		else \
+			printf "\033[31mError: $$file.out is not the same\033[0m\n"; \
+			failedtest=`expr $$failedtest + 1`; \
+			failedtestnames="$$failedtestnames:$$file"; \
+	    fi; \
+	done ; \
+	if [ $$failedtest != 0 ]; then \
+		echo "\033[31m$$failedtest tests failed\033[0m"; \
+		echo "These tests failed: $$failedtestnames:"; \
+		exit 1; \
+	fi
+
+EXTRA_DIST = $(TEST_FILES) \
+	$(TEST_FILES:%.in=%.out) 
+
+CLEANFILES=xyz
