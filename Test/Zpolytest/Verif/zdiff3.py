4ccc984abc117fd75d05431455ee15983b976d03
Vincent Loechner <loechner@unistra.fr>
2025-10-02 13:27:20 +0200

    update in/out

--- Test/Zpolytest/Verif/zdiff3.py
+++ Test/Zpolytest/Verif/zdiff3.py
@@ -18,10 +18,9 @@ print("diff = A-B contains", len(diff), "points")
 
 ZP = set()
 ZP = ZP.union(ZP2D("(2*i, j)", "1", "100", "(4*i+301)/3", "2*i+100"))
-ZP = ZP.union(ZP2D("(2*i, j)", "1", "2", "(2*i+1)", "2*i+100"))
 ZP = ZP.union(ZP2D("(200, j)", "1", "1", "201", "300"))
-ZP = ZP.union(ZP2D("(6*i+2, j)", "1", "32", "6*i+3", "(4*i+101)"))
-ZP = ZP.union(ZP2D("(6*i+4, j)", "1", "32", "6*i+5", "(4*i+102)"))
+ZP = ZP.union(ZP2D("(6*i+2, j)", "0", "33", "6*i+3", "6*i+102"))
+ZP = ZP.union(ZP2D("(6*i+4, j)", "0", "32", "6*i+5", "6*i+104"))
 
 print("len(ZP) =", len(ZP))
 if len(ZP - diff) == 0:
@@ -40,10 +39,10 @@ else:
 diff2 = B - A
 print("diff2 = B-A contains", len(diff2), "points")
 YP = set()
-YP = YP.union(ZP2D("(3*i, j)", "0", "100", "2*i+1", "3*i"))
+YP = YP.union(ZP2D("(3*i, j)", "0", "100", "2*i+1", "3*i")) #
 YP = YP.union(ZP2D("(3*i, j)", "67", "100", "2*i+1", "2*i+100"))
 YP = YP.union(ZP2D("(3, j)", "0", "0", "3", "102"))
-YP = YP.union(ZP2D("(6*i+3, j)", "1", "32", "6*i+4", "4*i+102"))
+YP = YP.union(ZP2D("(6*i+3, j)", "0", "49", "4*i+3", "4*i+102"))
 print("len(YP) =", len(YP))
 if len(YP - diff2) == 0:
   print("YP is in diff2")
