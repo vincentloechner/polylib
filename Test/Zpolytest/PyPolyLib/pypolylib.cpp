5cb54dd54cb1f6c18ad40926f89b8b988c6c4f97
sabrinabougarech@gmail.com <sabrinabougarech@gmail.com>
2026-06-02 16:40:35 +0200

    correction de quelques erreurs de copier/coller et commande de compilation

--- Test/Zpolytest/PyPolyLib/pypolylib.cpp
+++ Test/Zpolytest/PyPolyLib/pypolylib.cpp
@@ -1,12 +1,28 @@
+
+// Commande de compilation de ce fichier source en un module python :
+// (à partir du répertoire Test/Zpolytest/PyPolyLib/)
+// c++ -Wall -shared -std=c++11 -fPIC pypolylib.cpp ../../../.libs/libpolylibgmp.so $(python3 -m pybind11 --includes) -o pypolylib$(python3-config --extension-suffix) -I../../../include
+// cette commande génère un fichier 'pypolylib.cpython-312-x86_64-linux-gnu.so' qui peut être utilisé comme un module python.
+// 
+// Ensuite pour lancer python3 il faut ajouter l'emplacement où trouver libpolylibgmp.so dans cette variable d'environnement :
+// export LD_LIBRARY_PATH=$HOME/polylib/.libs/
+// 
+// Puis on peut lancer :
+// python3
+// >>> import pypolylib
+// >>> a = pypolylib.LBL()
+
+
 #include <pybind11/pybind11.h>
 
+#include <gmpxx.h>
 extern "C" {
 #include "polylib/polylibgmp.h"
 }
 
 namespace py = pybind11;
 
-PYBIND11MODULE(pypolylib, m) { 
+PYBIND11_MODULE(pypolylib, m) { 
     
     py::class_<Matrix>(m, "Matrix")
         .def_readonly("nbrows", &Matrix::NbRows)
@@ -14,17 +30,17 @@ PYBIND11MODULE(pypolylib, m) {
     m.def("MatrixAlloc", &Matrix_Alloc);
 
 
-    py::class<Polyhedron>(m, "Polyhedron")
+    py::class_<Polyhedron>(m, "Polyhedron")
         .def_readonly("dimension", &Polyhedron::Dimension)
         .def_readonly("nbconstraints", &Polyhedron::NbConstraints)
         .def_readonly("nbrays", &Polyhedron::NbRays);
     m.def("Constraints2Polyhedron", &Constraints2Polyhedron);
 
 
-    py::class<LBL>(m, "LBL")
+    py::class_<LBL>(m, "LBL")
         .def(py::init<>())
         .def_readwrite("Lat", &LBL::Lat)
-        .def_readwrite("P", &LBL::P);
+        .def_readwrite("P", &LBL::P)
         .def_readwrite("next", &LBL::next);
         
 
