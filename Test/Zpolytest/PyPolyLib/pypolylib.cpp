d7589cdaf3cbc0f953c1f4c4bbde1a6668b19365
aitkhorsa <siham.aitkhorsa@outlook.fr>
2026-06-03 10:36:10 +0200

    modifie

--- Test/Zpolytest/PyPolyLib/pypolylib.cpp
+++ Test/Zpolytest/PyPolyLib/pypolylib.cpp
@@ -1,51 +1,94 @@
-
 // Commande de compilation de ce fichier source en un module python :
 // (à partir du répertoire Test/Zpolytest/PyPolyLib/)
-// c++ -Wall -shared -std=c++11 -fPIC pypolylib.cpp ../../../.libs/libpolylibgmp.so $(python3 -m pybind11 --includes) -o pypolylib$(python3-config --extension-suffix) -I../../../include
-// cette commande génère un fichier 'pypolylib.cpython-312-x86_64-linux-gnu.so' qui peut être utilisé comme un module python.
-// 
-// Ensuite pour lancer python3 il faut ajouter l'emplacement où trouver libpolylibgmp.so dans cette variable d'environnement :
+// c++ -Wall -shared -std=c++11 -fPIC pypolylib.cpp ../../../.libs/libpolylibgmp.so /
+//     $(python3 -m pybind11 --includes) /
+//     -o pypolylib$(python3-config --extension-suffix) /
+//     -I../../../include
+//
 // export LD_LIBRARY_PATH=$HOME/polylib/.libs/
-// 
-// Puis on peut lancer :
 // python3
 // >>> import pypolylib
 // >>> a = pypolylib.LBL()
 
 
+
+//import pypolylib
+//# Créer une matrice 2x3
+//m = pypolylib.MatrixAlloc(2, 3)
+//print(m.nbrows, m.nbcolumns)  # -> 2 3
+
+
+
+
+#include <gmpxx.h>   // DOIT être avant polylibgmp.h
 #include <pybind11/pybind11.h>
 
-#include <gmpxx.h>
 extern "C" {
 #include "polylib/polylibgmp.h"
 }
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pypolylib, m) { 
-    
-    py::class_<Matrix>(m, "Matrix")
-        .def_readonly("nbrows", &Matrix::NbRows)
+// --- Destructeur personnalisé pour LBL ---
+struct LBLDeleter {
+    void operator()(LBL* p) const {
+        if (p) LBLFree(p);
+    }
+};
+using LBLPtr = std::unique_ptr<LBL, LBLDeleter>;
+
+// --- Idem pour Matrix et Polyhedron ---
+struct MatrixDeleter {
+    void operator()(Matrix* p) const {
+        if (p) Matrix_Free(p);
+    }
+};
+using MatrixPtr = std::unique_ptr<Matrix, MatrixDeleter>;
+
+struct PolyhedronDeleter {
+    void operator()(Polyhedron* p) const {
+        if (p) Polyhedron_Free(p);
+    }
+};
+using PolyhedronPtr = std::unique_ptr<Polyhedron, PolyhedronDeleter>;
+
+
+PYBIND11_MODULE(pypolylib, m) {
+
+    // --- Matrix ---
+    py::class_<Matrix, MatrixPtr>(m, "Matrix")
+        .def_readonly("nbrows",    &Matrix::NbRows)
         .def_readonly("nbcolumns", &Matrix::NbColumns);
-    m.def("MatrixAlloc", &Matrix_Alloc);
+
+    m.def("MatrixAlloc", [](unsigned nbrows, unsigned nbcols) {
+        return MatrixPtr(Matrix_Alloc(nbrows, nbcols));
+    }, py::arg("nbrows"), py::arg("nbcols"));
 
 
-    py::class_<Polyhedron>(m, "Polyhedron")
-        .def_readonly("dimension", &Polyhedron::Dimension)
-        .def_readonly("nbconstraints", &Polyhedron::NbConstraints)
-        .def_readonly("nbrays", &Polyhedron::NbRays);
-    m.def("Constraints2Polyhedron", &Constraints2Polyhedron);
+    // --- Polyhedron ---
+    py::class_<Polyhedron, PolyhedronPtr>(m, "Polyhedron")
+        .def_readonly("dimension",      &Polyhedron::Dimension)
+        .def_readonly("nbconstraints",  &Polyhedron::NbConstraints)
+        .def_readonly("nbrays",         &Polyhedron::NbRays);
+
+    m.def("Constraints2Polyhedron", [](Matrix *m, unsigned flags) {
+        return PolyhedronPtr(Constraints2Polyhedron(m, flags));
+    }, py::arg("matrix"), py::arg("flags") = 0);
 
 
-    py::class_<LBL>(m, "LBL")
-        .def(py::init<>())
-        .def_readwrite("Lat", &LBL::Lat)
-        .def_readwrite("P", &LBL::P)
-        .def_readwrite("next", &LBL::next);
-        
+    // --- LBL ---
+    py::class_<LBL, LBLPtr>(m, "LBL")
+        .def_property_readonly("Lat",
+            [](LBL &l) { return l.Lat; },
+            py::return_value_policy::reference)
+        .def_property_readonly("P",
+            [](LBL &l) { return l.P; },
+            py::return_value_policy::reference)
+        .def_property_readonly("next",
+            [](LBL &l) { return l.next; },
+            py::return_value_policy::reference);
 
-    m.def("LBLAlloc", &LBLAlloc);
-
-    
+    m.def("LBLAlloc", [](Matrix *lat, Polyhedron *domain) {
+        return LBLPtr(LBLAlloc(lat, domain));
+    }, py::arg("lat"), py::arg("domain"));
 }
-
