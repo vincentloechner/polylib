0e7f0c305d0b538e964bffc0f2caeb79b6928013
Vincent Loechner <loechner@unistra.fr>
2026-07-02 13:21:34 +0200

    rename

---
+++ PyPolyLib/examples/ZPre1.py
@@ -0,0 +1,16 @@
+import pypolylib
+from pypolylib import LBLRead, Transfo
+
+# Preimage of a Z-polyhedron
+# inspired from ZPre1.in
+
+# LBL = (2i, j) where 1 <= i,j <= 100
+a = LBLRead("{(2i, j) | 1 <= i <= 100, 1 <= j <= 100}")
+print("a =", a)
+
+# fonction (i,j) -> (2i+10, 3i+j+1)
+f = Transfo("(i,j -> 2i+10, 3i+j+1)")
+
+# preimage
+b = a.preimage(f)
+print("preimage =", b)
