568460cd1b8547475d5a1c9b0cb49c9d2a5b87e9
Vincent Loechner <loechner@unistra.fr>
2026-07-06 18:06:07 +0200

    basic tests of the python library

---
+++ PyPolyLib/tests/test_pypolylib.py
@@ -0,0 +1,52 @@
+# basic pypolylib tests
+
+from pypolylib import LBL, Transfo
+
+"""--------------------- Universe/empty LBL ----------------------"""
+univ = LBL("{(x,y)|}")
+assert str(univ) == "{(i, j) | }"
+empty = LBL("{(x,y)|1 < 0}")
+assert str(empty) == "{(i, j) | <empty>}"
+
+### ERROR : empty LBL has not necessarily an HNF lat !!!
+
+"""--------------------- Basic LBL manipulation ---------------------"""
+a = LBL("{(2i, 2i+j) | 1 <= i <= 5, 1 <= j <= i}")
+b = LBL("{(i,j) | j < 5}")
+assert str(a) == "{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0}"
+assert str(b) == "{(i, j) | j <= 4}"
+
+# a is part of the universe, and empty is part of a:
+assert a in univ
+assert empty in a
+
+# union
+assert str(a + b) == """{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0} UNION
+  {(i, j) | j <= 4}"""
+assert a+b == b.union(a) == a.union(b)
+
+# difference
+assert str(a - b) == "{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0, j >= 5}"
+assert a-b == a.difference(b)
+
+# intersection
+assert str(a * b) == "{(2i, j) | -2i+j - 1 >= 0, 3i-j >= 0, j <= 4}"
+assert a*b == a.intersection(b)
+
+# Z-domain
+assert a.zdomain() == a
+
+"""--------------------- Iterate over LBLs ---------------------"""
+sA = set(a)
+assert len(sA) == 15
+for z in a:
+  assert z in sA
+
+"""--------------------- Tranformations ---------------------"""
+t = Transfo("(i,j -> 3i+1, i+j)")
+assert str(t) == "(i, j -> 3i+1, i+j)"
+c = a.image(t)      # c = image of a under t
+d = c.preimage(t)   # preimage of c under f
+assert d == a
+
+print("all tests passed")
