e63b194234d5f4f9314ccae13043067b4c892992
Vincent Loechner <loechner@unistra.fr>
2026-07-07 12:52:19 +0200

    improved test messages

--- PyPolyLib/tests/test_ZDomain.py
+++ PyPolyLib/tests/test_ZDomain.py
@@ -6,7 +6,7 @@ import traceback
 from pypolylib import LBL
 
 
-# (nom, lbl_string)
+# (name, lbl)
 # Vérification : a.included(zdomain(a)) and zdomain(a).included(a)
 tests = [
     ("empty    ",
@@ -30,18 +30,18 @@ tests = [
     # from Pugh91 (the Omega test nightmare)
     ("ZDomain12",
      "{(i, j) | 11i+13j>=27, 11i+13j<=45, 7i-9j+10>=0, -7i+9j+4>=0}"),
+]
 
-
-    # pretty long ones below, not tested by default:
-
+# pretty long ones below, not tested by default:
+longtests = [
     # this one is from Rui-Juan Jing, Marc Moreno Maza, et al.,
     # Quantifier Elimination Over the Integers
     # https://dl.acm.org/doi/pdf/10.1145/3747199.3747580
-    # ("ZDomain13",
-    #  "{(x1, x2) | -98877x1 - 189663x2 - 1798x3 <= 705915, -10109x1 - 5958x2 - 14601x3 <= 31333, -5405x1 + 4965x2 + 3870x3 <= 4303504, 729x1 - 117x2 + 350x3 <= 4561, 677x1 + 465x2 - 540x3 <= 3489}"),
+    ("ZDomain13",
+     "{(x1, x2) | -98877x1 - 189663x2 - 1798x3 <= 705915, -10109x1 - 5958x2 - 14601x3 <= 31333, -5405x1 + 4965x2 + 3870x3 <= 4303504, 729x1 - 117x2 + 350x3 <= 4561, 677x1 + 465x2 - 540x3 <= 3489}"),
 
-    # ("ZDomain11",
-    #  "{(2i, 0, 52i+66j) | 198k <= -150i+j, 264k >= -200i+j, 9j <= 2i, 5j >= i, i <= 1000}"),
+    ("ZDomain11",
+     "{(2i, 0, 52i+66j) | 198k <= -150i+j, 264k >= -200i+j, 9j <= 2i, 5j >= i, i <= 1000}"),
 ]
 
 passed = 0
@@ -50,13 +50,13 @@ failed = 0
 for name, lbl_str in tests:
     a = None
     b = None
-    print(f"{name}... ", end="", flush=True)
+    print(f"{name} ", end="", flush=True)
     try:
         a = LBL(lbl_str)
         b = a.zdomain()
         assert a.included(b), "a not included in zdomain(a)"
         assert b.included(a), "zdomain(a) not included in a"
-        # less precise output, but basically same computation:
+        # less precise output, but basically it's the same computation:
         # assert a == b
 
         print("OK")
--- PyPolyLib/tests/test_pypolylib.py
+++ PyPolyLib/tests/test_pypolylib.py
@@ -3,14 +3,16 @@
 from pypolylib import LBL, Transfo
 
 """--------------------- Universe/empty LBL ----------------------"""
+print(f"Universe/empty LBL     ", end="", flush=True)
+
 univ = LBL("{(x,y)|}")
 assert str(univ) == "{(i, j) | }"
 empty = LBL("{(x,y)|1 < 0}")
 assert str(empty) == "{(i, j) | <empty>}"
-
-### ERROR : empty LBL has not necessarily an HNF lat !!!
+print("OK")
 
 """--------------------- Basic LBL manipulation ---------------------"""
+print(f"2D LBL                 ", end="", flush=True)
 a = LBL("{(2i, 2i+j) | 1 <= i <= 5, 1 <= j <= i}")
 b = LBL("{(i,j) | j < 5}")
 assert str(a) == "{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0}"
@@ -19,30 +21,53 @@ assert str(b) == "{(i, j) | j <= 4}"
 # a is part of the universe, and empty is part of a:
 assert a in univ
 assert empty in a
+print("OK")
 
+# operations
+print(f"Operations on 2D LBL   ", end="", flush=True)
 # union
-assert str(a + b) == """{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0} UNION
+u = a + b
+assert str(u) == """{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0} UNION
   {(i, j) | j <= 4}"""
-assert a+b == b.union(a) == a.union(b)
+assert u == b.union(a) == a.union(b)
 
 # difference
-assert str(a - b) == "{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0, j >= 5}"
-assert a-b == a.difference(b)
+d = a - b
+assert str(d) == "{(2i, j) | i <= 5, -2i+j - 1 >= 0, 3i-j >= 0, j >= 5}"
+assert d == a.difference(b)
 
 # intersection
-assert str(a * b) == "{(2i, j) | -2i+j - 1 >= 0, 3i-j >= 0, j <= 4}"
-assert a*b == a.intersection(b)
+i = a * b
+assert str(i) == "{(2i, j) | -2i+j - 1 >= 0, 3i-j >= 0, j <= 4}"
+assert i == a.intersection(b)
+
+# diff * inter = {0}; diff + inter = a
+assert d * i == empty
+assert d + i == a
 
 # Z-domain
 assert a.zdomain() == a
+print("OK")
 
 """--------------------- Iterate over LBLs ---------------------"""
+print(f"Enumerate LBL          ", end="", flush=True)
 sA = set(a)
 assert len(sA) == 15
 for z in a:
   assert z in sA
+print("OK")
+
+"""--------------------- Transformations ---------------------"""
+print(f"LBL transformations    ", end="", flush=True)
+t = Transfo("(i,j -> 3i+1, i+j)")
+assert str(t) == "(i, j -> 3i+1, i+j)"
+c = a.image(t)      # c = image of a under t
+d = c.preimage(t)   # preimage of c under f
+assert d == a
+print("OK")
 
 """--------------------- Errors over LBLs ---------------------"""
+print(f"Errors over LBLs       ", end="", flush=True)
 # incompatible dimensions
 c = LBL("{(i, j, k) | 0 <= i <=5, 0 <= j <= i, 0 <= k <= i}")  
 try: a+c; assert False
@@ -53,18 +78,13 @@ try: list(b); assert False
 except ValueError: pass
 
 # parse error
-try: d = LBL("{nothing}"); assert False
+try: d = LBL("{g4r|o4ge}"); assert False
 except ValueError: pass
 
 # parse error (UTF-8)
 try: d = LBL("{(x)|x≤2}"); assert False
 except ValueError: pass
+print("OK")
 
-"""--------------------- Transformations ---------------------"""
-t = Transfo("(i,j -> 3i+1, i+j)")
-assert str(t) == "(i, j -> 3i+1, i+j)"
-c = a.image(t)      # c = image of a under t
-d = c.preimage(t)   # preimage of c under f
-assert d == a
-
-print("all tests passed")
+"""--------------------- All done ---------------------"""
+# print("all tests passed")
