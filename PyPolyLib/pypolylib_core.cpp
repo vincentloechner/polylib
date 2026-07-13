4f9c1535a4a5e38c2ee99f4a5b3e950b82148924
Vincent Loechner <loechner@unistra.fr>
2026-07-13 21:04:43 +0200

    cleanup

--- PyPolyLib/pypolylib_core.cpp
+++ PyPolyLib/pypolylib_core.cpp
@@ -1,18 +1,3 @@
-// Commande de compilation de ce fichier source en un module python :
-// (à partir du répertoire Test/Zpolytest/PyPolyLib/)
-// c++ -Wall -shared -std=c++11 -fPIC pypolylib_core.cpp ../../../.libs/libpolylibgmp.so    $(python3 -m pybind11 --includes) -o pypolylib_core$(python3-config --extension-suffix)      -I../../../include
-// c++ -Wall -shared -std=c++11 -fPIC pypolylib.cpp ../../../.libs/libpolylibgmp.so /
-//     $(python3 -m pybind11 --includes) /
-//     -o pypolylib$(python3-config --extension-suffix) /
-//     -I../../../include
-// 
-// export LD_LIBRARY_PATH=$HOME/polylib/.libs/
-// python3
-// >>> import pypolylib
-// >>> a = pypolylib.LBL()
-
-
-
 
 #include <gmpxx.h>   // MUST come before polylibgmp.h
 #include <pybind11/pybind11.h>
