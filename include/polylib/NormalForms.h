9df6c8da5ead4c22f18f5e1976f99d08a4724276
Vincent Loechner <loechner@unistra.fr>
2026-08-28 15:47:43 +0200

    restore Hermite and Smith (used by Barvinok)

--- include/polylib/NormalForms.h
+++ include/polylib/NormalForms.h
@@ -2,8 +2,8 @@
 extern "C" {
 #endif
 
-// extern void Smith(Matrix *A, Matrix **U, Matrix **V, Matrix **Product);
-// extern void Hermite(Matrix *A, Matrix **H, Matrix **U);
+extern void Smith(Matrix *A, Matrix **U, Matrix **V, Matrix **Product);
+extern void Hermite(Matrix *A, Matrix **H, Matrix **U);
 
 #if defined(__cplusplus)
 }
